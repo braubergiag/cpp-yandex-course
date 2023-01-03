24a0affa7344531562cfbedb5923bd6ddaa7723a
Igor Glushkov <braubergiag@gmail.com>
2023-01-03 22:54:25 +0300

    complete submodule 1.4

---
+++ 02_yellow/module-01/assignments/04_task_manager/04_task_manager.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include "TaskManager.h"
+using namespace std;
+// Перечислимый тип для статуса задачи
+
+
+void PrintTasksInfo(TasksInfo tasks_info) {
+	cout << tasks_info[TaskStatus::NEW] << " new tasks" <<
+		", " << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress" <<
+		", " << tasks_info[TaskStatus::TESTING] << " tasks are being tested" <<
+		", " << tasks_info[TaskStatus::DONE] << " tasks are done" << endl;
+}
+
+
+
+int main() {
+
+	TeamTasks tasks;
+	tasks.AddNewTask("Ilia");
+	for (int i = 0; i < 3; ++i) {
+		tasks.AddNewTask("Ivan");
+	}
+	cout << "Ilia's tasks: ";
+	PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+	cout << "Ivan's tasks: ";
+	PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
+
+	TasksInfo updated_tasks, untouched_tasks;
+
+	tie(updated_tasks, untouched_tasks) =
+		tasks.PerformPersonTasks("Ivan", 2);
+	cout << "Updated Ivan's tasks: ";
+	PrintTasksInfo(updated_tasks);
+	cout << "Untouched Ivan's tasks: ";
+	PrintTasksInfo(untouched_tasks);
+
+	tie(updated_tasks, untouched_tasks) =
+		tasks.PerformPersonTasks("Ivan", 2);
+	cout << "Updated Ivan's tasks: ";
+	PrintTasksInfo(updated_tasks);
+	cout << "Untouched Ivan's tasks: ";
+	PrintTasksInfo(untouched_tasks);
+
+	return 0;
+}

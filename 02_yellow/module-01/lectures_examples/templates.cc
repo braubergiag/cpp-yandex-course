b46ef8872ee345cf0d3f86f2dcc84145f6b691dc
Igor Glushkov <braubergiag@gmail.com>
2023-01-05 16:29:19 +0300

    example from 1.5 submodule tests

--- 02_yellow/module-01/lectures_examples/templates.cc
+++ 02_yellow/module-01/lectures_examples/templates.cc
@@ -3,54 +3,19 @@
 #include <vector>
 #include <map>
 #include <utility>
+#include <array>
 using namespace std;
 
-template <typename T>
-ostream& operator<<(ostream& os, const vector<T> & v ){
-    os << "[";
-    for (const auto & el : v){
-        os << el << ",";
-    }
-    os << "]";
-
-    return os;
-}
-
-
-template <typename First, typename Second>
-ostream& operator<<(ostream & os, const pair<First,Second> p){
-    os << "(" << p.first << "," << p.second << ")";
-    return os;
-}
-
-
-template <typename Key, typename Value>
-ostream& operator<<(ostream & os, const map<Key,Value> & m){
-    os << "{";
-    for (const auto & el : m){
-        os  << el  << ",";
-    }
-    os << "}";
-
-    return os;
-}
-
-template <typename T>
-T Max(const T& a, const T& b){
-    return a > b ? a : b;
-}
-
+#include "../assignments/07_containers_os.h"
 
 int main(){
 
-#if 0
-    vector<int> v {12,3,3};
-    cout << v << endl;
 
-    map<int,int> m {{1,2},{3,4}};
-    cout << m << endl;
-#endif
-    cout << Max<int>(2,3.4);
+    vector<int> v{1,2,3};
+    map<int,int> m{{1,2},{2,3},{4,5}};
+    array<int,5> ar{0,2,3,4,5};
+    vector<pair<string,int>> vv{{"Igor",26},{"Kate",28}};
+    cout << vv;
 
 }
 

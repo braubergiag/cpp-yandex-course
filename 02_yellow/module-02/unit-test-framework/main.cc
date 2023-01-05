88bdf10cd9a856040c797a9ea99bf988cc578970
Igor Glushkov <braubergiag@gmail.com>
2023-01-05 23:04:38 +0300

    unit test framework refactoring

--- 02_yellow/module-02/unit-test-framework/main.cc
+++ 02_yellow/module-02/unit-test-framework/main.cc
@@ -1,75 +1,15 @@
-#include "containers_os.h"
-#include <sstream>
-
-
-
-template <typename U , typename V>
-void AssertEqual(const U & u, const V & v){
-    stringstream ss;
-    if (u != v){
-        ss << u << " != " << v;
-        throw runtime_error(ss.str());
-    }
-
-}
-
-void Assert(bool val){
-    if (!val){
-        throw runtime_error("failed.");
-    }
-}
-
-class TestRunner {
-
-public:
-    TestRunner() = default;
-
-
-    template <typename Function>
-    void RunTest(Function test,const string& hint){
-        try
-        {
-            test();
-            ++passed_tests_;
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << hint << " " << e.what() << '\n';
-            ++failed_tests_;
-        }
-        
-    }
-
-    ~TestRunner(){
-        PrintStatistics();
-    }
-
-    void PrintStatistics(){
-        cout << "=================\n"
-             << "Test passed: " << passed_tests_ << '\n'
-             << "Test failed: " << failed_tests_ << '\n';
-        
-    }
-
-
-
-private:
-    int failed_tests_{0};
-    int passed_tests_{0};
-
-
-};
-
+#include "test_runner.h"
 
 
 void TestAddition(){
-    AssertEqual(1,2);
+    AssertEqual(2,2, "TestEqual 1");
 }
 
 void TestCompare(){
-    Assert(2*2 == 4);
-    Assert(2*2 == 5);
+    Assert(2*2 == 4, "Test compare 1");
+    Assert(2*2 == 5, "Test compare 2");
 }
+
 
 
 int main(){
@@ -77,6 +17,7 @@ int main(){
     TestRunner runner;
     runner.RunTest(TestAddition,"TestAddition");
     runner.RunTest(TestCompare,"TestCompare");
+    runner.RunTest(TestAddition,"TestAddition");
 
     return 0;
 }

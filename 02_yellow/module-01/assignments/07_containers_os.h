ad7cf74cf62a90c5a9d5efe2c8871e041692ce93
Igor Glushkov <braubergiag@gmail.com>
2023-01-05 16:27:59 +0300

    complete example from 1.5 submodule

--- 02_yellow/module-01/assignments/07_containers_os.h
+++ 02_yellow/module-01/assignments/07_containers_os.h
@@ -4,45 +4,47 @@
 #include <map>
 #include <utility>
 #include <array>
+#include <sstream>
 using namespace std;
 
+
+
+template <typename Collection>
+std::string Printer(const Collection & collection, char sep){
+
+    stringstream ss;
+    bool first{true};
+    for (const auto & el: collection){
+        if (!first) {
+            ss << sep;
+        }
+        first = false;
+        ss << el;
+    }
+
+
+    return ss.str();
+    
+}
 template <typename T>
 ostream& operator<<(ostream& os, const vector<T> & v ){
-    os << "[";
-    for (const auto & el : v){
-        os << el << ",";
-    }
-    os << "]";
-
-    return os;
+   return os << "[" << Printer(v,',') << "]";
 }
 
 
 template <typename T, size_t size>
-ostream& operator<<(ostream& os, const array<T,size> & v ){
-    os << "<";
-    for (const auto & el : v){
-        os << el << ",";
-    }
-    os << ">";
-
-    return os;
+ostream& operator<<(ostream& os, const array<T,size> & a ){
+    return os << "<" << Printer(a,',') << ">";
 }
 
 template <typename First, typename Second>
 ostream& operator<<(ostream & os, const pair<First,Second> p){
-    os << "(" << p.first << "," << p.second << ")";
-    return os;
+    return os << "(" << p.first << "," << p.second << ")";
 }
 
 
 template <typename Key, typename Value>
 ostream& operator<<(ostream & os, const map<Key,Value> & m){
-    os << "{";
-    for (const auto & el : m){
-        os  << el  << ",";
-    }
-    os << "}";
+    return os << "{" << Printer(m,',') << "}";
 
-    return os;
 }

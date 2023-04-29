906053d8e4784b4c67c6fac699ee5fbfae8fa9ae
Igor Glushkov <braubergiag@gmail.com>
2023-04-29 21:23:38 +0300

    YB fix find_nearest_element algorithm

--- 02-yb/m-04/hw/mod-04/find_nearest_element.cc
+++ 02-yb/m-04/hw/mod-04/find_nearest_element.cc
@@ -1,22 +1,21 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-// #include "test_runner.h"
+ #include "test_runner.h"
 #include <set>
 using namespace std;
 
 set<int>::const_iterator FindNearestElement(const set<int> & elements, int border){
     if (elements.empty()) return end(elements);
-    auto [lb,ub] = elements.equal_range(border);
-
-    if (*lb == border) return lb;
-    else lb = prev(lb);
-
-    if (abs(border - *lb) <= abs(border - *ub)) return lb;
-    else return ub;
+    auto lb = elements.lower_bound(border);
+    if (lb == end(elements)) {
+        return prev(lb);
+    } else if (lb != begin(elements) && (abs(*prev(lb) - border) <= abs(*lb - border))) {
+        return prev(lb);
+    } else return lb;
 
 }
-#if 0
+#if 1
 void test_1(){
     set<int> v{1,2,4,5,7,9,10,14,15};
     ASSERT_EQUAL(*FindNearestElement(v,0),1);
@@ -42,10 +41,32 @@ void test_empty(){
      ASSERT(FindNearestElement(v,2) == end(v));
 }
 #endif
+
+
+void test_border_is_largest_element_in_range(){
+    set<int> numbers = {1, 4, 6};
+    ASSERT_EQUAL(*FindNearestElement(numbers,8),6);
+    ASSERT_EQUAL(*FindNearestElement(numbers,6),6);
+}
+void test_border_is_smallest_element_in_range(){
+    set<int> numbers {1,4,6};
+    ASSERT_EQUAL(*FindNearestElement(numbers,-2),1);
+    ASSERT_EQUAL(*FindNearestElement(numbers,1),1);
+
+}
+void test_border_in_the_middle_of_range(){
+    set<int> numbers {1,4,6};
+    ASSERT_EQUAL(*FindNearestElement(numbers,2),1);
+    ASSERT_EQUAL(*FindNearestElement(numbers,3),4);
+
+}
 int main(){
 
-#if 0
+#if 1
     TestRunner tr;
+    RUN_TEST(tr, test_border_is_largest_element_in_range);
+    RUN_TEST(tr, test_border_is_smallest_element_in_range);
+    RUN_TEST(tr, test_border_in_the_middle_of_range);
     RUN_TEST(tr,test_1);
     RUN_TEST(tr,test_empty);
 #endif 

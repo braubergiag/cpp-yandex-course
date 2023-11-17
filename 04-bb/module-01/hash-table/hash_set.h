70615b519c01a67c33d25370af3665cc29027468
glushkov <glushkov@protei-lab.ru>
2023-11-17 13:04:13 +0300

    complete hash-table task

--- 04-bb/module-01/hash-table/hash_set.h
+++ 04-bb/module-01/hash-table/hash_set.h
@@ -1,14 +1,22 @@
-#include "test_runner.h"
+// #include "../test_runner.h"
 
 #include <forward_list>
 #include <iterator>
+#include <algorithm>
+#include <vector>
+/*
+Требования:
+В любой момент времени в вашей хеш-таблице должно быть ровно num_buckets корзин. Рехеширование выполнять не нужно.
+Метод Add не должен менять содержимое таблицы, если элемент, равный данному, уже есть в таблице.
+Метод Has должен возвращать true тогда и только тогда, когда элемент, равный данному, есть в таблице.
+Метод Erase должен стирать элемент, равный данному, из таблицы, если он там есть, в противном случае он не должен менять содержимое таблицы.
+Метод GetBucket должен возвращать ссылку на бакет, который содержал бы данный элемент, вне зависимости от того, есть он в таблице или нет.
 
-using namespace std;
-
+*/
 template <typename Type, typename Hasher>
 class HashSet {
 public:
-  using BucketList = forward_list<Type>;
+  using BucketList = std::forward_list<Type>;
 
 public:
   explicit HashSet(
@@ -19,95 +27,55 @@ public:
   void Add(const Type& value);
   bool Has(const Type& value) const;
   void Erase(const Type& value);
+  size_t GetBucketIndex(const Type& value) const;
   const BucketList& GetBucket(const Type& value) const;
+
+private:
+    size_t num_buckets_;
+    std::vector<BucketList> buckets_;
+    Hasher hasher_;
 };
 
-struct IntHasher {
-  size_t operator()(int value) const {
-    // Это реальная хеш-функция из libc++, libstdc++.
-    // Чтобы она работала хорошо, std::unordered_map
-    // использует простые числа для числа бакетов
-    return value;
-  }
-};
-
-struct TestValue {
-  int value;
-
-  bool operator==(TestValue other) const {
-    return value / 2 == other.value / 2;
-  }
-};
-
-struct TestValueHasher {
-  size_t operator()(TestValue value) const {
-    return value.value / 2;
-  }
-};
-
-void TestSmoke() {
-  HashSet<int, IntHasher> hash_set(2);
-  hash_set.Add(3);
-  hash_set.Add(4);
-
-  ASSERT(hash_set.Has(3));
-  ASSERT(hash_set.Has(4));
-  ASSERT(!hash_set.Has(5));
-
-  hash_set.Erase(3);
-
-  ASSERT(!hash_set.Has(3));
-  ASSERT(hash_set.Has(4));
-  ASSERT(!hash_set.Has(5));
-
-  hash_set.Add(3);
-  hash_set.Add(5);
-
-  ASSERT(hash_set.Has(3));
-  ASSERT(hash_set.Has(4));
-  ASSERT(hash_set.Has(5));
+template<typename Type, typename Hasher>
+size_t HashSet<Type, Hasher>::GetBucketIndex(const Type &value) const {
+    return hasher_(value) % num_buckets_;
 }
 
-void TestEmpty() {
-  HashSet<int, IntHasher> hash_set(10);
-  for (int value = 0; value < 10000; ++value) {
-    ASSERT(!hash_set.Has(value));
-  }
+template<typename Type, typename Hasher>
+HashSet<Type, Hasher>::HashSet(size_t num_buckets, const Hasher &hasher) :
+    num_buckets_(num_buckets), buckets_(num_buckets), hasher_(hasher) {}
+
+template<typename Type, typename Hasher>
+void HashSet<Type, Hasher>::Add(const Type &value) {
+    if (!Has(value)){
+        auto bucket_index = hasher_(value) % num_buckets_;
+        buckets_[bucket_index].push_front(value);
+    }
 }
 
-void TestIdempotency() {
-  HashSet<int, IntHasher> hash_set(10);
-  hash_set.Add(5);
-  ASSERT(hash_set.Has(5));
-  hash_set.Add(5);
-  ASSERT(hash_set.Has(5));
-  hash_set.Erase(5);
-  ASSERT(!hash_set.Has(5));
-  hash_set.Erase(5);
-  ASSERT(!hash_set.Has(5));
+template<typename Type, typename Hasher>
+bool HashSet<Type, Hasher>::Has(const Type &value) const {
+    auto bucket_index = GetBucketIndex(value);
+    if (bucket_index < buckets_.size()){
+        auto & bucket = buckets_[bucket_index];
+        if (std::find(begin(bucket), end(bucket), value) != end(bucket))
+            return true;
+    }
+
+    return false;
 }
 
-void TestEquivalence() {
-  HashSet<TestValue, TestValueHasher> hash_set(10);
-  hash_set.Add(TestValue{2});
-  hash_set.Add(TestValue{3});
-
-  ASSERT(hash_set.Has(TestValue{2}));
-  ASSERT(hash_set.Has(TestValue{3}));
-
-  const auto& bucket = hash_set.GetBucket(TestValue{2});
-  const auto& three_bucket = hash_set.GetBucket(TestValue{3});
-  ASSERT_EQUAL(&bucket, &three_bucket);
-
-  ASSERT_EQUAL(1, distance(begin(bucket), end(bucket)));
-  ASSERT_EQUAL(2, bucket.front().value);
+template<typename Type, typename Hasher>
+void HashSet<Type, Hasher>::Erase(const Type &value) {
+    if (Has(value)){
+        auto bucket_index = GetBucketIndex(value);
+        buckets_[bucket_index].remove(value);
+    }
 }
 
-int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestSmoke);
-  RUN_TEST(tr, TestEmpty);
-  RUN_TEST(tr, TestIdempotency);
-  RUN_TEST(tr, TestEquivalence);
-  return 0;
+template<typename Type, typename Hasher>
+const typename HashSet<Type, Hasher>::BucketList &HashSet<Type, Hasher>::GetBucket(const Type &value) const {
+    auto bucket_index = GetBucketIndex(value);
+    return buckets_[bucket_index];
 }
+

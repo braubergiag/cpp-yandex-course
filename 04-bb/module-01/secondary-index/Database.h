0fe536c2802142b98f0efc445372d1d2f9968887
glushkov <glushkov@protei-lab.ru>
2023-11-18 14:43:10 +0300

    init secondary-index db

---
+++ 04-bb/module-01/secondary-index/Database.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+using std::string;
+
+
+
+struct Record {
+    string id;
+    string title;
+    string user;
+    int timestamp;
+    int karma;
+};
+
+// Реализуйте этот класс
+class Database {
+public:
+    bool Put(const Record& record);
+    const Record* GetById(const string& id) const;
+    bool Erase(const string& id);
+
+    template <typename Callback>
+    void RangeByTimestamp(int low, int high, Callback callback) const;
+
+    template <typename Callback>
+    void RangeByKarma(int low, int high, Callback callback) const;
+
+    template <typename Callback>
+    void AllByUser(const string& user, Callback callback) const;
+};
